34774cd1cdab21d4333bf4ec652b6160814cc476
mcmoe <holla@mcmoe.com>
2012-12-16 23:18:02 +0100

    Java Test + JUnit Maven Dependency  + Maven Site
    First java test class with its corresponding test class + Junit
    Dependency for Maven + generation of Maven Site

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+	
+	public static void main(String [] args) {
+		
+		if(Test.returnTrue()) {
+			System.out.println("Hello World!");
+		}
+	}
+	public static boolean returnTrue() {
+		return true;
+	}
+
+}
---
+++ src/test/java/TestTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+
+public class TestTest extends TestCase {
+
+	public void testReturnTrue() {
+		boolean returnValue = Test.returnTrue();
+		assertTrue(returnValue);
+	}
+}
