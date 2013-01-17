2ccfa91f7d0b2fae6a5ceb7e1b7cc793307fb970
mcmoe <mcxmoe@gmail.com>
2013-01-17 02:05:38 +0100

    completing relist engine code. reslit automation of all items on all pages now available.

--- jsikulifut13/src/main/java/engine/ReList.java
+++ jsikulifut13/src/main/java/engine/ReList.java
@@ -5,52 +5,95 @@ import org.sikuli.api.ScreenRegion;
 
 public class ReList {
 	
+	int page;
 	Controller controller;
 	TradePileRegion tradePileRegion;
 	
 	public ReList(Controller controller) {
+		page = 1;
 		this.controller = controller;
 		tradePileRegion = new TradePileRegion(controller.getScreen());
 	}
 	
-	public void reList() {
+	private void navigateToTradePile() {
+		// navigate to trade pile		
+		System.out.println("looking for Trading..."); // TODO: logger
+		ScreenRegion trading = tradePileRegion.findTradingTab(5000); 
+		// this should be worked around as to not have to wait 7000 milliseconds to ensure refresh after login
+        
+		if(trading != null) {
+			System.out.println("clicking on trading..."); // TODO: logger
+			controller.clickCenterOf(trading);
+		}
+
+		System.out.println("looking for Trade Pile..."); // TODO: logger	
+        ScreenRegion tradePileTab = tradePileRegion.findTradePileTab(5000);
+        
+        if(tradePileTab != null) {
+        	System.out.println("clicking on tradePile..."); // TODO: logger
+        	controller.clickCenterOf(tradePileTab);
+        }
+	}
+	
+	private void reListCurrentPage() {
+        System.out.println("waiting for xpired..."); // TODO: logger      
+        ScreenRegion expiredItem = tradePileRegion.findXpiredItem(3000);
+         
+        while(expiredItem != null) {
+	        controller.clickCenterOf(expiredItem);
+	        
+	        System.out.println("waiting for list button..."); // TODO: logger
+	        ScreenRegion relistButton = tradePileRegion.findReListButton(3000);
+	        
+	        while(relistButton != null) {
+	        	controller.clickCenterOf(relistButton);
+	        	relistButton = tradePileRegion.findReListButton(1000);
+	        }
+	        
+	        System.out.println("waiting for more xpired..."); // TODO: logger
+	        expiredItem = tradePileRegion.findXpiredItem(3000);
+        }
+	}
+	
+	private boolean nextPage() {
+		boolean hasNextPage = false;
+		ScreenRegion nextPage = tradePileRegion.findNextPage(1000);
 		
+		if(nextPage != null) {
+			hasNextPage = true;
+			controller.clickCenterOf(nextPage);
+			page++;
+		}
+		
+		return hasNextPage;
+	}
+	
+	private void reListAllPages() {
+		do {
+			reListCurrentPage();
+		} while (nextPage());
+	}
+	
+	private void navigateToFirstPage() {
+		while(page > 1) {
+			ScreenRegion previousPage = tradePileRegion.findPreviousPage(1000);
+			
+			if(previousPage != null) {
+				controller.clickCenterOf(previousPage);
+				page--;
+			}
+		}
+	}
+	
+	public void reList() {	
 		// navigate to trade pile		
 		System.out.println("looking for Coins/Points to ensure proper login..."); // TODO: logger
-		if(tradePileRegion.findCoinsPointsRegion(5000) != null) {
-			
-			// navigate to trade pile		
-			System.out.println("looking for Trading..."); // TODO: logger
-			ScreenRegion trading = tradePileRegion.findTradingTab(5000); 
-			// this should be worked around as to not have to wait 7000 milliseconds to ensure refresh after login
-	        
-			if(trading != null) {
-				System.out.println("clicking on trading..."); // TODO: logger
-				controller.clickCenterOf(trading);
-			}
-	
-			System.out.println("looking for Trade Pile..."); // TODO: logger	
-	        ScreenRegion tradePileTab = tradePileRegion.findTradePileTab(5000);
-	        
-	        if(tradePileTab != null) {
-	        	System.out.println("clicking on tradePile..."); // TODO: logger
-	        	controller.clickCenterOf(tradePileTab);
-	        }
-	        
-	        System.out.println("waiting for xpired..."); // TODO: logger      
-	        ScreenRegion expiredItem = tradePileRegion.findXpiredItem(5000);
-	         
-	        if(expiredItem != null) {
-		        controller.clickCenterOf(expiredItem);
-		        
-		        System.out.println("waiting for list button..."); // TODO: logger
-		        ScreenRegion relistButton = tradePileRegion.findReListButton(5000);
-		        
-		        controller.clickCenterOf(relistButton);
-	        }
+		if(tradePileRegion.findCoinsPointsRegion(20000) != null) {		
+			navigateToTradePile();	        
+			reListAllPages();
+			navigateToFirstPage();
 		}
         
         System.out.println("exiting reList..."); // TODO: logger
 	}
-
 }
--- jsikulifut13/src/main/java/gui/fut/TradePileRegion.java
+++ jsikulifut13/src/main/java/gui/fut/TradePileRegion.java
@@ -12,7 +12,12 @@ public class TradePileRegion {
 	Target tradePileTab;
 	Target expiredItem;
 	Target buttonList;
+	Target highlightedButtonList;
 	Target coinsPointsRegion;
+	Target activeNextPage;
+	Target highlightedNextPage;
+	Target activePreviousPage;
+	Target highlightedPreviousPage;
 	
 	public TradePileRegion(ScreenRegion screen) {
 		this.screen = screen;
@@ -22,7 +27,12 @@ public class TradePileRegion {
 		tradePileTab = new ImageTarget(targetClass.getResource("/TradePile.png")); 
 		expiredItem = new ImageTarget(targetClass.getResource("/Xpired.png"));
 		buttonList = new ImageTarget(targetClass.getResource("/ButtonList.png"));
+		highlightedButtonList = new ImageTarget(targetClass.getResource("/ButtonHighlightedList.png"));
 		coinsPointsRegion = new ImageTarget(targetClass.getResource("/CoinsFIFAPoints.png"));
+		activeNextPage = new ImageTarget(targetClass.getResource("/ArrowActiveRight.png"));
+		highlightedNextPage = new ImageTarget(targetClass.getResource("/ArrowActiveHighlightedRight.png"));
+		activePreviousPage = new ImageTarget(targetClass.getResource("/ArrowActiveLeft.png"));
+		highlightedPreviousPage = new ImageTarget(targetClass.getResource("/ArrowActiveHighlightedLeft.png"));
 	}
 	
 	public ScreenRegion findTradingTab(int millis) {
@@ -38,10 +48,30 @@ public class TradePileRegion {
 	}
 
 	public ScreenRegion findReListButton(int millis) {
-        return screen.wait(buttonList, millis); 
+		ScreenRegion button = screen.wait(buttonList, millis/2);	
+		if(button == null) {
+			button = screen.wait(highlightedButtonList, millis/2);	
+		}
+		return button;
 	}
 
 	public ScreenRegion findCoinsPointsRegion(int millis) {
 		return screen.wait(coinsPointsRegion, millis); 
+	}
+	
+	public ScreenRegion findNextPage(int millis) {
+		ScreenRegion nextPage = screen.wait(activeNextPage, millis/2);	
+		if(nextPage == null) {
+			nextPage = screen.wait(highlightedNextPage, millis/2);	
+		}
+		return nextPage;
+	}
+	
+	public ScreenRegion findPreviousPage(int millis) {
+		ScreenRegion previousPage = screen.wait(activePreviousPage, millis/2);	
+		if(previousPage == null) {
+			previousPage = screen.wait(highlightedPreviousPage, millis/2);	
+		}
+		return previousPage;
 	}
 }
